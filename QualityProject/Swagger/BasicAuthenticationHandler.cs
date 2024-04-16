fbb414c149f09f733662434c82793571157e927d
Tomas Zouhar <xzouha13@vutbr.cz>
2024-04-16 10:42:17 +0200

    Auth values not hardcoded

--- QualityProject/Swagger/BasicAuthenticationHandler.cs
+++ QualityProject/Swagger/BasicAuthenticationHandler.cs
@@ -1,61 +1,67 @@
+using System;
 using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 
-namespace QualityProject.API.Swagger;
-
-public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+namespace QualityProject.API.Swagger
 {
-    
-
-    public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
     {
-    }
+        private readonly IConfiguration _configuration;
 
-    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
-    {
-        if (!Request.Headers.ContainsKey("Authorization"))
+        public BasicAuthenticationHandler(
+            IConfiguration configuration,
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder)
+            : base(options, logger, encoder)
         {
-            return Task.FromResult(AuthenticateResult.Fail("Missing Authorization Header"));
+            _configuration = configuration;
         }
 
-        string authorizationHeader = Request.Headers["Authorization"]!;
-        if (string.IsNullOrEmpty(authorizationHeader))
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            return Task.FromResult(AuthenticateResult.Fail("Missing Authorization Header"));
-        }
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Missing Authorization Header"));
+            }
 
-        if (!authorizationHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
-        {
-            return Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
-        }
+            string authorizationHeader = Request.Headers["Authorization"]!;
+            if (string.IsNullOrEmpty(authorizationHeader))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Missing Authorization Header"));
+            }
 
-        string encodedUsernamePassword = authorizationHeader.Substring("Basic ".Length).Trim();
-        Encoding encoding = Encoding.GetEncoding("iso-8859-1");
-        string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+            if (!authorizationHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
+            }
 
-        int seperatorIndex = usernamePassword.IndexOf(':');
-        string username = usernamePassword.Substring(0, seperatorIndex);
-        string password = usernamePassword.Substring(seperatorIndex + 1);
+            string encodedUsernamePassword = authorizationHeader.Substring("Basic ".Length).Trim();
+            Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+            string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
 
-        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-        {
-            return Task.FromResult(AuthenticateResult.Fail("Invalid Username or Password"));
-        }
+            int seperatorIndex = usernamePassword.IndexOf(':');
+            string username = usernamePassword.Substring(0, seperatorIndex);
+            string password = usernamePassword.Substring(seperatorIndex + 1);
 
-        if (username == "admin" && password == "admin")
-        {
+            string validUsername = _configuration["BasicAuth:Username"];
+            string validPassword = _configuration["BasicAuth:Password"];
+
+            if (username != validUsername || password != validPassword)
+            {
+                Console.WriteLine(username + validUsername);
+                Console.WriteLine(password + validPassword);
+                return Task.FromResult(AuthenticateResult.Fail("Invalid Username or Password"));
+            }
+
             var claims = new[] { new Claim(ClaimTypes.Name, username) };
             var identity = new ClaimsIdentity(claims, Scheme.Name);
             var principal = new ClaimsPrincipal(identity);
             var ticket = new AuthenticationTicket(principal, Scheme.Name);
             return Task.FromResult(AuthenticateResult.Success(ticket));
-        }
-        else
-        {
-            return Task.FromResult(AuthenticateResult.Fail("Invalid Username or Password"));
         }
     }
 }
