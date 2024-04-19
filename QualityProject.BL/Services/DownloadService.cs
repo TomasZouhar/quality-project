6a64b1450b0efa1ade5efb690ccf2da1da890334
SamValastin <samuel.valastin@datasentics.com>
2024-04-19 19:00:39 +0200

    Revert "Add: DownloadService tests & refactor Download Service (and fix its using in CompareService)"
    This reverts commit 2664f9a9cd6deee70d8300377d3482cd6cd15919.

--- QualityProject.BL/Services/DownloadService.cs
+++ QualityProject.BL/Services/DownloadService.cs
@@ -2,20 +2,15 @@ namespace QualityProject.BL.Services;
 
 public class DownloadService : IDownloadService
 {
-    private readonly HttpClient _httpClient;
-
-    public DownloadService(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
-
     public async Task<string> DownloadFileAsync()
     {
+        using var httpClient = new HttpClient();
         const string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3";
-        _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
         const string url = "https://ark-funds.com/wp-content/uploads/funds-etf-csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv";
-        var response = await _httpClient.GetAsync(url);
+        var response = await httpClient.GetAsync(url);
         response.EnsureSuccessStatusCode();
+
         return await response.Content.ReadAsStringAsync();
     }
 }
--- QualityProject/Program.cs
+++ QualityProject/Program.cs
@@ -47,7 +47,6 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-builder.Services.AddHttpClient<IDownloadService, DownloadService>();
 builder.Services.AddScoped<ICompareService, CompareService>();
 builder.Services.AddScoped<IDownloadService, DownloadService>();
 builder.Services.AddScoped<IFormatService, FormatService>();
